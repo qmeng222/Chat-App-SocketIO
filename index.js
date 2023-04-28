44c3564542990193285491f47faae9d3eab6233e
Qingying Meng <qmeng222@gmail.com>
2023-04-27 21:33:30 -0700

    worked on front-end & fetched the input value

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // server side:
 // matching "main": "index.js" in `package.json`
-const http = require("http");
-const express = require("express");
 
+const express = require("express");
 // create an instance of the Express application:
 const app = express();
 
+const http = require("http");
 // create an HTTP server that listens for requests on the "app":
 const expressServer = http.createServer(app);
 
@@ -13,6 +13,11 @@ const expressServer = http.createServer(app);
 const { Server } = require("socket.io");
 // create a new instance of the Server class (on backend), and initialize it with the expressServer instance:
 const io = new Server(expressServer);
+
+// listen for and handle connection events on the Socket.IO server (a server-side library):
+io.on("connection", (socket) => {
+  console.log("New user connected.");
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
